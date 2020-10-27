e8bd7752b8460d0b5d1d444f88a7486b627d9829
Alessio Milanese <milanese.alessio@gmail.com>
2020-10-27 22:23:49 +0100

    add main script

---
+++ util/fish_probes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+try:
+    from util import load_input, predict_probes
+except ImportError:
+    sys.exit("CRITICAL ERROR: Unable to find the util python package.")
+
+import time
+
+def main():
+    t0 = time.time()
+
+    # load the input files
+    sequences,taxonomy,sel_clade,min_len = load_input.load_and_check_input()
+    # find probes
+    predict_probes.predict_probes(sequences,taxonomy,sel_clade,min_len)
+
+    sys.stderr.write('Elapsed time: {} s\n'.format( (time.time()-t0) ) )
+
+if __name__ == '__main__':
+    main()
