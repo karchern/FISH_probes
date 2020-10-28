199ce61becdc2d2361aa4066c44e27abf474a31d
Alessio Milanese <milanese.alessio@gmail.com>
2020-10-28 12:38:39 +0100

    improve readibility of the code

--- fish_probes/load_input.py
+++ fish_probes/load_input.py
@@ -1,13 +1,34 @@
 import sys
 import argparse
 
+# ------------------------------------------------------------------------------
+# Function to parse the input
+# ------------------------------------------------------------------------------
 def input_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('sequences', action="store", default=None, help='File containing the sequences')
-    parser.add_argument('taxonomy', action="store", default=None, help='File containing the taxonomy')
-    parser.add_argument('sel_clade', action="store", default=None, help='Clade selected to design the primers')
-    parser.add_argument('-v', action='store', type=int, default=3, dest='verbose', help='Verbose level: 1=error, 2=warning, 3=message, 4+=debugging [3]')
-    parser.add_argument('-m', action='store', type=int, default=20, dest='min_len', help='Minimum probe length [20]')
+
+    # File with the sequences in fasta format
+    parser.add_argument('sequences', action="store", default=None,
+                        help='File containing the sequences')
+
+    # File with the taxonomy, the taxonomy is separated by ";", and there is a
+    # "\t" between name and tax (Example: "gene1\tclade1;clade2;clade3\n")
+    parser.add_argument('taxonomy', action="store", default=None,
+                        help='File containing the taxonomy')
+
+    # Clade for which we have to find the primers
+    parser.add_argument('sel_clade', action="store", default=None,
+                        help='Clade selected to design the primers')
+
+    # Verbose level
+    parser.add_argument('-v', action='store', type=int, default=3,
+                        dest='verbose', help='Verbose level: 1=error,'\
+                        ' 2=warning, 3=message, 4+=debugging [3]')
+
+    # Minimum length of the probes that we identify
+    parser.add_argument('-m', action='store', type=int, default=20,
+                        dest='min_len', help='Minimum probe length [20]')
+
     args = parser.parse_args()
     return args
 
@@ -16,11 +37,12 @@ def input_parser():
 # ------------------------------------------------------------------------------
 # The purpouse of this function is to:
 #  - print the available commands
-#  - load the input from sys.argv
-#  - check that the input is correct
-#  - return the objects created
+#  - parse the arguments
+#  - load the input from the files and check that it is correct
+#  - return the created objects
 def load_and_check_input():
     # load sys.argv
     args = input_parser()
+    # check that the input is correct
     sys.stderr.write("check \n")
     return 1,2,3,4
