e093d7899d27e169f1b1a85437226f8d45b7cc0a
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-11 10:29:20 +0200

    change code to find all possible k-mers

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 Set of scripts to identify FISH probes in 16S sequences.
-The probes are unique for the selected clade. 
+The probes are unique for the selected clade.
+
+First clone the repo:
+```
+git clone https://github.com/AlessioMilanese/FISH_probes.git
+```
+
+Setup:
+```
+cd FISH_probes
+python setup.py bdist_wheel
+pip install --no-deps --force-reinstall dist/*.whl
+```
--- fish_probes/predict_probes.py
+++ fish_probes/predict_probes.py
@@ -16,44 +16,44 @@ def split_sequences(taxonomy,sel_clade,sequences):
 # ------------------------------------------------------------------------------
 # Find conserved regions in the sequences from the selected clade
 # ------------------------------------------------------------------------------
-def find_conserved_regions(sequences, min_len):
-    # to return
-    all_conserved_position = list()
+def DnaCheck(sequence):
+    return all(base.upper() in ('A', 'C', 'T', 'G', 'U') for base in sequence)
 
-    # we assume that we have a MSA and that the positions are aligned
-    len_ali = len(sequences[list(sequences.keys())[1]])
-    start_ali_tmp = -1
-    # we go through each position and see if it is conserved
-    for pos in range(len_ali):
-        is_pos_conserved = True
-        nucleotide_found = ""
-        for seq in sequences:
-            if nucleotide_found == "":
-                nucleotide_found = sequences[seq][pos]
-            else:
-                if nucleotide_found != sequences[seq][pos]:
-                    is_pos_conserved = False
-        # if is_pos_conserved is True, then all sequences have the same
-        # nucleotide in this position
-        # a) set up the start
-        if is_pos_conserved:
-            if start_ali_tmp == -1:
-                start_ali_tmp = pos
-        # b) save (previous) region if it is not conserved anymore
-        if not is_pos_conserved:
-            if start_ali_tmp != -1:
-                if pos - start_ali_tmp >= min_len:
-                    all_conserved_position.append([start_ali_tmp,pos,
-                                             sequences[seq][start_ali_tmp:pos]])
-                start_ali_tmp = -1
-        # c) case of the last position being conserved
-        if is_pos_conserved and pos == len_ali-1:
-            if start_ali_tmp != -1:
-                if (pos+1) - start_ali_tmp >= min_len:
-                    all_conserved_position.append([start_ali_tmp,(pos+1),
-                                         sequences[seq][start_ali_tmp:(pos+1)]])
+def find_kmers(string,k):
+    f = {}
+    for x in range(len(string)+1-k):
+        kmer = string[x:x+k]
+        # we select only if they contain nucleotide sequences
+        if DnaCheck(kmer):
+            f[kmer] = f.get(kmer, 0) + 1
+    return f
 
-    return all_conserved_position
+def find_conserved_regions(list_strings,k):
+    all_strings_kmers = dict()
+    for s in list_strings:
+        all_strings_kmers[s] = find_kmers(s,k)
+    # find all possible k-mers
+    all_kmers = set()
+    for s in all_strings_kmers:
+        for kmer in all_strings_kmers[s]:
+            all_kmers.add(kmer)
+    # now we count how many times it appear
+    count_mers = dict()
+    for kmer in list(all_kmers):
+        count_mers[kmer] = 0
+    # now we add the counts
+    for s in all_strings_kmers:
+        for kmer in all_strings_kmers[s]:
+            count_mers[kmer] = count_mers[kmer] + 1
+    # return
+    return count_mers
+
+# ------------------------------------------------------------------------------
+# Starting from the conserved regions, check if they are unique
+# ------------------------------------------------------------------------------
+def check_uniqueness(all_conserved_position, seq_other, probe_len):
+    print("ok")
+
 
 # ------------------------------------------------------------------------------
 # Main function
@@ -62,14 +62,17 @@ def find_conserved_regions(sequences, min_len):
 #  - sequences, dictionary of seq_id -> nucleotide sequence
 #  - taxonomy, dictionary of seq_id -> "clade1;clade2;clade3"
 #  - sel_clade, clade for which we have to design the probe
-#  - min_len, minimul length for the selected probe (positive integer)
+#  - probe_len, length for the selected probe (positive integer)
 #  - verbose,
 #  - outfile, where to save the output. If None, then stdout
-def predict_probes(sequences,taxonomy,sel_clade,min_len,verbose,outfile):
+def predict_probes(sequences,taxonomy,sel_clade,probe_len,verbose,outfile):
     # Zero, find sequences that belong to the selected clade
     seq_sel_clade, seq_other = split_sequences(taxonomy,sel_clade,sequences)
 
     # First, identify possible conserved regions
-    all_conserved_position = find_conserved_regions(seq_sel_clade,min_len)
+    all_sel_kmers = find_conserved_regions(list(seq_sel_clade.values()),\
+                                                probe_len)
+
     # Second, check if identified regions are unique, compared to the other
     # clades
+    check_uniqueness(all_sel_kmers,seq_other,probe_len)
