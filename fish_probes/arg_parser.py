49da4615e8b38c701ced339a522aff04dd696f0c
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-20 23:43:17 +0200

    refine the args handling

--- fish_probes/arg_parser.py
+++ fish_probes/arg_parser.py
@@ -13,16 +13,16 @@ def input_parser():
                         help='command',choices=['design','test_probe'])
 
     # File with the sequences in fasta format
-    parser.add_argument('sequences', action="store", default=None,
+    parser.add_argument('-s', dest = 'sequences', action="store", default=None,
                         help='File containing the sequences')
 
     # File with the taxonomy, the taxonomy is separated by ";", and there is a
     # "\t" between name and tax (Example: "gene1\tclade1;clade2;clade3\n")
-    parser.add_argument('taxonomy', action="store", default=None,
+    parser.add_argument('-t', dest = 'taxonomy', action="store", default=None,
                         help='File containing the taxonomy')
 
     # Clade for which we have to find the primers
-    parser.add_argument('sel_clade', action="store", default=None,
+    parser.add_argument('-c', dest = 'sel_clade', action="store", default=None,
                         help='Clade selected to design the primers')
 
     # Verbose level
@@ -52,12 +52,33 @@ def input_parser():
     # CHECK ARGUMENTS
     # check args verbose
     if args.verbose < 1:
-        log.print_error("Verbose (-v) needs to be higher than 0",2)
+        log.print_error("Verbose (-v) needs to be higher than 0")
     # check args perc seq
     if args.perc_seq < 0 or args.perc_seq > 1:
-        log.print_error("Threshold (-p) should be between 0 and 1",2)
+        log.print_error("Threshold (-p) should be between 0 and 1")
     # check length of the probe
     if args.probe_len < 1:
         log.print_error("Probe length (-k) cannot be lower than 0")
+
+
+    ############################################################################
+    # CHECK ARGUMENTS FOR COMMAND DESIGN
+    if args.command == "design":
+        # there are three mandatory input
+        if args.sequences is None:
+            log.print_error("Missing -s.")
+        if args.taxonomy is None:
+            log.print_error("Missing -t.")
+        if args.sel_clade is None:
+            log.print_error("Missing -c.")
+
+    ############################################################################
+    # CHECK ARGUMENTS FOR COMMAND TEST_PROBE
+    if args.command == "test_probe":
+        # there is only one mandatory input
+        if args.input is None:
+            log.print_error("Missing -i.")
+
+
 
     return args
