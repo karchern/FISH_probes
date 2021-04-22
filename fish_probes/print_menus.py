ab98435761bd3ccbe36db02d94c1d46619c67609
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-22 14:50:42 +0200

    add colours to the menus

--- fish_probes/print_menus.py
+++ fish_probes/print_menus.py
@@ -34,23 +34,76 @@ def main_message(name=None):
 # design menu
 # ------------------------------------------------------------------------------
 def design():
+    # HEADER -------------------------------------------------------------------
     sys.stderr.write("\n")
-    sys.stderr.write("Usage: fish_probes design -s <seq> -t <tax> -c CLADE [options]\n\n")
-    sys.stderr.write("Options:\n")
-    sys.stderr.write("   -s  FILE    fasta file with the 16S sequences\n")
-    sys.stderr.write("   -t  FILE    file with the taxonomy for the 16S sequences provided in -s\n")
-    sys.stderr.write("   -c  STR     clade for which we need to find the probe\n")
-    sys.stderr.write("   -k  INT     length of the probe [20]\n")
-    sys.stderr.write("   -o          output file name [stdout]\n")
-    sys.stderr.write("   -p  FLOAT   minimum fraction of sequences that should contain the selected probe [0.9]\n")
-    sys.stderr.write("   -v  INT     verbose level: 1=error, 2=warning, 3=message, 4+=debugging [3]\n\n")
+    sys.stderr.write(log.colour("Usage: ", "cyan"))
+    sys.stderr.write("fish_probes design ")
+    sys.stderr.write(log.colour("-s ", "blue"))
+    sys.stderr.write("<seq> ")
+    sys.stderr.write(log.colour("-t ", "blue"))
+    sys.stderr.write("<tax> ")
+    sys.stderr.write(log.colour("-c ", "blue"))
+    sys.stderr.write("<clade>")
+
+    sys.stderr.write(" [option]\n\n")
+
+    # PARAMETERS ---------------------------------------------------------------
+    sys.stderr.write(log.colour("   -s  ","blue"))
+    sys.stderr.write("FILE    ")
+    sys.stderr.write("fasta file with the 16S sequences")
+    sys.stderr.write(log.colour("\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -t  ","blue"))
+    sys.stderr.write("FILE    ")
+    sys.stderr.write("file with the taxonomy for the 16S sequences provided in -s")
+    sys.stderr.write(log.colour("\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -c  ","blue"))
+    sys.stderr.write("STR     ")
+    sys.stderr.write("clade for which we need to find the probe")
+    sys.stderr.write(log.colour("\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -k  ","blue"))
+    sys.stderr.write("INT     ")
+    sys.stderr.write("length of the probe")
+    sys.stderr.write(log.colour(" [20]\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -o  ","blue"))
+    sys.stderr.write("FILE    ")
+    sys.stderr.write("output file name")
+    sys.stderr.write(log.colour(" [stdout]\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -p  ","blue"))
+    sys.stderr.write("FLOAT    ")
+    sys.stderr.write("minimum fraction of sequences that should contain the selected probe")
+    sys.stderr.write(log.colour(" [0.9]\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -v  ","blue"))
+    sys.stderr.write("INT     ")
+    sys.stderr.write("verbose level: 1=error, 2=warning, 3=message, 4+=debugging")
+    sys.stderr.write(log.colour(" [3]\n", "magenta"))
+
 
 # ------------------------------------------------------------------------------
 # test_probe menu
 # ------------------------------------------------------------------------------
 def test_probe():
+    # HEADER -------------------------------------------------------------------
     sys.stderr.write("\n")
-    sys.stderr.write("Usage: fish_probes test_probe -i SEQUENCE [options]\n\n")
-    sys.stderr.write("Options:\n")
-    sys.stderr.write("   -i  STR   probe to test\n")
-    sys.stderr.write("   -v  INT   verbose level: 1=error, 2=warning, 3=message, 4+=debugging [3]\n\n")
+    sys.stderr.write(log.colour("Usage: ", "cyan"))
+    sys.stderr.write("fish_probes test_probe ")
+    sys.stderr.write(log.colour("-i ", "blue"))
+    sys.stderr.write("<probe_seq> ")
+
+    sys.stderr.write(" [option]\n\n")
+
+    # PARAMETERS ---------------------------------------------------------------
+    sys.stderr.write(log.colour("   -i  ","blue"))
+    sys.stderr.write("STR    ")
+    sys.stderr.write("probe to test")
+    sys.stderr.write(log.colour("\n", "magenta"))
+
+    sys.stderr.write(log.colour("   -v  ","blue"))
+    sys.stderr.write("INT     ")
+    sys.stderr.write("verbose level: 1=error, 2=warning, 3=message, 4+=debugging")
+    sys.stderr.write(log.colour(" [3]\n", "magenta"))
