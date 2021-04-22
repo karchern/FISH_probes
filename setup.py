b70b4b96853c34f8627fdd6ed11fc98729a0298e
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-22 17:03:49 +0200

    solve problem with installation

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fish_probes',
     version='0.0.0',
     author='Alessio Milanese',
     author_email='milanese.alessio@gmail.com',
+    packages=find_packages(exclude=["test"]),
     entry_points={
         'console_scripts': [
             'fish_probes = fish_probes.MAIN:main',
