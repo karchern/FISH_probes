14d00a78047296e4ad304c2de6b5d9a859584268
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-22 21:29:38 +0200

    try to identify the position of the test

--- fish_probes/TEST.py
+++ fish_probes/TEST.py
@@ -1,4 +1,10 @@
 import sys
+import os
+import pkg_resources
+from fish_probes import UTIL_log
+
+TEST_DATA_PATH = pkg_resources.resource_filename("fish_probes", "test")
 
 def test():
-    print("tt")
+    UTIL_log.print_log("Run test")
+    print(TEST_DATA_PATH)
